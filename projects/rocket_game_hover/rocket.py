e47fd2932bd2a9c78f70239d9ef7ff3dfadad4c2
MarvinAhl <marvin_ahl@t-online.de>
2022-01-22 17:01:33 +0100

    BDQified the hover version of Rocket Game and made it ready for testing

---
+++ projects/rocket_game_hover/rocket.py
@@ -0,0 +1,131 @@
+import pygame
+import numpy as np
+import physics
+
+class Rocket:
+    def __init__(self):
+        self.l1 = 1.5  # Distance between center of mass and engine in m
+        self.l2 = 3.5  # Distance between center of mass and nose in m
+        self.m = 1.0E3  # Overall mass in kg
+        self.J = 5.25E3  # Moment of Inertia in kg * m^2
+
+        self.rng = np.random.default_rng()
+
+        self.reset()
+    
+    def reset(self):
+        # Coordinates are relative to landing pad in the bottom middle of the screen, y is pointing down
+        self.x = 0.0
+        self.y = -30.0
+        self.phi = 0.0
+
+        self.x_v = 0.0
+        self.y_v = 0.0
+        self.phi_v = 0.0
+        
+        self.f1 = 0.0
+        self.f2 = 0.0
+        self.theta = 0.0  # Angle of thrust
+
+        return np.array([self.x, self.y, self.x_v, self.y_v, self.phi, self.phi_v], dtype=np.float32)
+    
+    def init(self):
+        pass
+    
+    def update(self, dt):
+        x_a = self._calc_x_a()
+        self.x_v += x_a * dt
+        self.x += self.x_v * dt
+        
+        y_a = self._calc_y_a()
+        self.y_v += y_a * dt
+        self.y += self.y_v * dt
+
+        phi_a = self._calc_phi_a()
+        self.phi_v += phi_a * dt
+        self.phi += self.phi_v * dt
+
+        if self.phi > np.pi:
+            self.phi -= 2.0 * np.pi
+        elif self.phi <= -np.pi:
+            self.phi += 2.0 * np.pi
+        
+        # Return state
+        return np.array([self.x, self.y, self.x_v, self.y_v, self.phi, self.phi_v], dtype=np.float32)
+
+    def control(self, actions):
+        """
+        3 action:
+        Booster power: 0 off, 1 on
+        Booster angle: 0 left, 1 half-left, 2 center, 3 half-right, 4 right
+        Control Nozzle: 0 left, 1 off, 2 right
+        """
+        if actions[0] == 0:
+            self.set_f1(0.0)  # Turn off Booster
+        else:
+            self.set_f1(1.0)  # Turn on Booster
+        
+        if actions[1] == 0:
+            self.set_theta(1.0)  # Angle Booster left
+        elif actions[1] == 1:
+            self.set_theta(0.5)  # Angle Booster half-left
+        elif actions[1] == 2:
+            self.set_theta(0.0)  # Angle Booster center
+        elif actions[1] == 3:
+            self.set_theta(-0.5)  # Angle Booster half-right
+        else:
+            self.set_theta(-1.0)  # Angle Booster right
+
+        if actions[2] == 0:
+            self.set_f2(-1.0)  # Fire right control Nozzle
+        elif actions[2] == 1:
+            self.set_f2(0.0)  # Turn off Nozzle
+        else:
+            self.set_f2(1.0)  # Fire left control Nozzle
+
+    def set_f1(self, f):
+        self.f1 = physics.main_engine_force * f
+    
+    def set_f2(self, f):
+        self.f2 = physics.control_engine_force * f
+
+    def set_theta(self, t):
+        self.theta = physics.main_engine_angle * t
+    
+    def render(self):
+        x1 = self.x - self.l1 * np.sin(self.phi)
+        y1 = self.y + self.l1 * np.cos(self.phi) - physics.ground_height
+
+        x1_px = x1 * physics.pixel_per_meter
+        y1_px = y1 * physics.pixel_per_meter
+
+        x2 = self.x + self.l2 * np.sin(self.phi)
+        y2 = self.y - self.l2 * np.cos(self.phi) - physics.ground_height
+
+        x2_px = x2 * physics.pixel_per_meter
+        y2_px = y2 * physics.pixel_per_meter
+
+        # Booster flame endpoint
+        xb = x1 - 4.0 * np.sin(self.phi + self.theta) * self.f1 / physics.main_engine_force
+        yb = y1 + 4.0 * np.cos(self.phi + self.theta) * self.f1 / physics.main_engine_force
+
+        xb_px = xb * physics.pixel_per_meter
+        yb_px = yb * physics.pixel_per_meter
+
+        # Nozzle flame endpoint
+        xn = x2 - 1.0 * np.cos(self.phi) * self.f2 / physics.control_engine_force
+        yn = y2 - 1.0 * np.sin(self.phi) * self.f2 / physics.control_engine_force
+
+        xn_px = xn * physics.pixel_per_meter
+        yn_px = yn * physics.pixel_per_meter
+
+        return (x1_px, y1_px), (x2_px, y2_px), (xb_px, yb_px), (xn_px, yn_px)
+
+    def _calc_x_a(self):
+        return np.sin(self.phi + self.theta) * self.f1 / self.m
+
+    def _calc_y_a(self):
+        return physics.g - np.cos(self.phi + self.theta) * self.f1 / self.m
+
+    def _calc_phi_a(self):
+        return (self.f2 * self.l2 - np.sin(self.theta) * self.f1 * self.l1) / self.J
---
+++ projects/rocket_game_hover/rocket_game_hover.py
@@ -0,0 +1,339 @@
+import sys
+sys.path.insert(1, '../..')
+
+from bdq import BDQ
+
+import pygame
+import physics
+from rocket import Rocket
+
+import torch
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Game:
+    def __init__(self, render=True, agent_play=True, agent_train=True, agent_file='rocket_game_hover', save_episodes=100,
+                 step_limit=2000, device='cpu'):
+        self.running = True
+        self.display_surf = None
+        self.size = (self.width, self.height) = (1280, 720)
+
+        self.rocket = Rocket()
+        self.last_dirty_rects = []
+
+        self.agent_play = agent_play
+        self.agent_train = agent_train
+        self.agent_file = agent_file
+
+        if agent_play:
+            sampling_period = 0.01
+            lookahead_horizon = 5.0
+            gamma = np.exp(-sampling_period/lookahead_horizon)  # Calculate discount factor
+            new_actions_prob = 5 * sampling_period  # Calculate e-greedy new action probability
+            
+            self.agent = BDQ(6, (2, 5, 3), new_actions_prob=new_actions_prob, gamma=gamma, device=device)
+            
+            if not agent_train:
+                self.agent.load_net('nets/' + agent_file + '.net')
+        else:
+            self.agent = None
+        
+        self.x_target = 0.0
+        self.y_target = -30.0
+
+        self.last_shaping = None  # For potential based reward shaping
+        
+        self.save_episodes = save_episodes
+        self.step_limit = step_limit
+
+        self.render = render
+
+        # For measuring frame time
+        self.last_time_ms = 0
+
+    def _init(self):
+        pygame.init()
+        self.display_surf = pygame.display.set_mode(self.size, pygame.SCALED, vsync=1)
+        pygame.display.set_caption("Rocket Game")
+
+        self.rocket.init()
+
+    def _on_event(self, event):
+        if event.type == pygame.QUIT:
+            self.running = False
+        
+        # Controls for human play
+        if not self.agent_play:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.rocket.set_f1(1.0)  # Activate Booster
+                elif event.key == pygame.K_a:
+                    self.rocket.set_theta(1.0)  # Angle Booster left
+                elif event.key == pygame.K_d:
+                    self.rocket.set_theta(-1.0)  # Angle Booster right
+                elif event.key == pygame.K_LEFT:
+                    self.rocket.set_f2(-1.0)  # Fire right control nozzle
+                elif event.key == pygame.K_RIGHT:
+                    self.rocket.set_f2(1.0)  # Fire left control nozzle
+
+            elif event.type == pygame.KEYUP:
+                pressed_keys = pygame.key.get_pressed()
+                if event.key == pygame.K_SPACE:
+                    self.rocket.set_f1(0.0)  # Booster engine off
+                elif event.key == pygame.K_a:
+                    if pressed_keys[pygame.K_d]:
+                        self.rocket.set_theta(-1.0)  # Angle Booster right
+                    else:
+                        self.rocket.set_theta(0.0)  # Angle Booster center
+                elif event.key == pygame.K_d:
+                    if pressed_keys[pygame.K_a]:
+                        self.rocket.set_theta(1.0)  # Angle Booster left
+                    else:
+                        self.rocket.set_theta(0.0)  # Angle Booster center
+                elif event.key == pygame.K_LEFT:
+                    if pressed_keys[pygame.K_RIGHT]:
+                        self.rocket.set_f2(1.0)  # Fire left control nozzle
+                    else:
+                        self.rocket.set_f2(0.0)  # Stop firing control nozzle
+                elif event.key == pygame.K_RIGHT:
+                    if pressed_keys[pygame.K_LEFT]:
+                        self.rocket.set_f2(-1.0)  # Fire right control nozzle
+                    else:
+                        self.rocket.set_f2(0.0)  # Stop firing control nozzle
+
+    def _update(self, dt, actions):
+        if self.agent_play:
+            self.rocket.control(actions)
+
+        state = self.rocket.update(dt)
+
+        # Oberservation is like state but only with relative positions
+        obsv = state.copy()
+        obsv[0] -= self.x_target
+        obsv[1] -= self.y_target
+
+        shaping = self._calc_shaping(obsv)
+        reward = shaping - self.last_shaping
+        self.last_shaping = shaping
+
+        dist = np.sqrt(obsv[0]**2 + obsv[1]**2)
+        done = dist > 28.0
+
+        return obsv, reward, done
+
+    def _calc_shaping(self, state):
+        # Square potentials scaled to have about the same impact
+        position = -0.06 * (state[0]**2 + state[1]**2)
+        velocity = -0.12 * (state[2]**2 + state[3]**2)
+        angle = -35.0 * state[4]**2
+        ang_vel = -35.0 * state[5]**2
+
+        # Scaled importance
+        return position + velocity + angle + ang_vel
+
+    def _render(self):
+        ppm = physics.pixel_per_meter
+
+        dirty_rects = []  # Store alle areas that changed in here
+        self.display_surf.fill((255, 255, 255))
+
+        (r_x1, r_y1), (r_x2, r_y2), (r_xb, r_yb), (r_xn, r_yn) = self.rocket.render()
+
+        # The coordinate systems origin is at the bottom center of the screen
+        x1 = r_x1 + self.width / 2
+        y1 = r_y1 + self.height
+        x2 = r_x2 + self.width / 2
+        y2 = r_y2 + self.height
+
+        xb = r_xb + self.width / 2
+        yb = r_yb + self.height
+        xn = r_xn + self.width / 2
+        yn = r_yn + self.height
+
+        burn_line = pygame.draw.line(self.display_surf, (239, 151, 0), (x1, y1), (xb, yb), 3)
+        nozzle_line = pygame.draw.line(self.display_surf, (239, 151, 0), (x2, y2), (xn, yn), 1)
+
+        rocket_circ1 = pygame.draw.circle(self.display_surf, (0, 0, 0), (x1, y1), 3)
+        rocket_circ2 = pygame.draw.circle(self.display_surf, (0, 0, 0), (x2, y2), 3)
+        rocket_line = pygame.draw.line(self.display_surf, (0, 0, 0), (x1, y1), (x2, y2), 5)
+
+        # Ground and landing pad
+        pygame.draw.rect(self.display_surf, (0, 0, 0), (0, self.height - physics.ground_height * ppm, self.width, physics.ground_height * ppm))
+        pygame.draw.line(self.display_surf, (239, 151, 0), (self.width/2 - 10 * ppm, self.height - physics.ground_height * ppm), (self.width/2 + 10 * ppm, self.height - physics.ground_height * ppm), 5)
+
+        new_dirty_rects = [rocket_circ1, rocket_circ2, rocket_line, burn_line, nozzle_line]
+        dirty_rects = self.last_dirty_rects + new_dirty_rects
+
+        pygame.display.update(dirty_rects)
+
+        self.last_dirty_rects = new_dirty_rects
+
+    def _cleanup(self):
+        pygame.quit()
+
+    def play(self):
+        if self._init() == False:
+            self.running = False
+
+        episode_rewards = []
+        steps = []
+        final_obsvs = []
+
+        episode = 0
+
+        # Loop over all Episodes:
+        while self.running:
+            episode += 1
+            print(f'Episode {episode} started')
+
+            state, done = self.rocket.reset(), False
+
+            obsv = state.copy()
+            obsv[0] -= self.x_target
+            obsv[1] -= self.y_target
+
+            self.last_shaping = self._calc_shaping(obsv)  # Calculate initial potential
+
+            episode_reward = 0.0
+            step_count = 0
+            greedy_count = 0
+
+            self._init_time()
+            # Loop for one Episode:
+            while self.running and not done:
+                dt = 0.01  # Will always calculate 100 steps per second, even if realtime simulation is slower
+
+                for event in pygame.event.get():
+                    self._on_event(event)
+                
+                if self.agent_play and self.agent_train:
+                    actions = self.agent.act(obsv)  # Exploration strategy for training
+                elif self.agent_play and not self.agent_train:
+                    actions = self.agent.act_optimally(obsv)  # Optimal strategy for evaluation
+                elif not self.agent_play:
+                    actions = None  # No action because human inputs work with pygame events
+
+                next_obsv, reward, done = self._update(dt, actions)
+                
+                if self.agent_play and self.agent_train:
+                    self.agent.experience(obsv, actions, reward, next_obsv, done)
+                    self.agent.train()
+
+                if self.render:
+                    self._render()
+                    self._delay(dt)  # Wait in render mode after each calculation to ensure simulation is in real time
+                
+                obsv = next_obsv
+
+                episode_reward += reward
+                step_count += 1
+
+                # Stop Episode if time limit is reached
+                if step_count >= self.step_limit and self.agent_play:
+                    done = True
+            
+            if self.agent_play:
+                print(f'Reward: {episode_reward}')
+            else:
+                print(f'x: {obsv[0]}')
+                print(f'y: {obsv[1]}')
+                print(f'x_v: {obsv[2]}')
+                print(f'y_v: {obsv[3]}')
+                print(f'phi: {obsv[4]}')
+                print(f'phi_v: {obsv[5]}')
+                print(f'Reward: {episode_reward}')
+                print('')
+
+            episode_rewards.append(episode_reward)
+            overall_steps = step_count if len(steps) < 1 else steps[-1] + step_count
+            steps.append(overall_steps)
+            final_obsvs.append(obsv)
+
+            if (episode % self.save_episodes == 0 or not self.running) and self.agent_play:
+                # Save Net if agent was trained
+                if self.agent_train:
+                    self.agent.save_net('nets/' + self.agent_file + f'_e{episode}' + '.net')
+
+                # Plot results
+                plt.clf()  # To prevent overlapping of old plots
+
+                figure, axis = plt.subplots(3, 1)
+                figure.suptitle('Training Stats')
+
+                axis[0].plot(np.arange(episode) + 1, episode_rewards, 'k-')
+                axis[0].grid(True)
+                axis[0].set_ylabel('Reward')
+
+                episode_steps = [steps[0]]
+                episode_steps += [steps[i+1] - steps[i] for i in range(episode-1)]
+                axis[1].plot(np.arange(episode) + 1, episode_steps, 'k-')
+                axis[1].grid(True)
+                axis[1].set_ylabel('Episode Steps')
+
+                axis[2].plot(np.arange(episode) + 1, steps, 'k-')
+                axis[2].grid(True)
+                axis[2].set_ylabel('Cumulative Steps')
+                axis[2].set_xlabel('Episodes')
+
+                prefix = 'train' if self.agent_train else 'eval'
+                plt.savefig('stats/' + prefix + f'_stats_e{episode}.png')
+
+                plt.close(figure)
+
+                # Plot final obsvs
+                plt.clf()  # To prevent overlapping of old plots
+
+                figure, axis = plt.subplots(3, 2)
+                figure.suptitle('Final Observations')
+
+                axis[0, 0].plot(np.arange(episode) + 1, [obsv[0] for obsv in final_obsvs], 'k.')
+                axis[0, 0].grid(True)
+                axis[0, 0].set_ylabel('x/y Position')
+
+                axis[0, 1].plot(np.arange(episode) + 1, [obsv[1] for obsv in final_obsvs], 'k.')
+                axis[0, 1].grid(True)
+
+                axis[1, 0].plot(np.arange(episode) + 1, [obsv[2] for obsv in final_obsvs], 'k.')
+                axis[1, 0].grid(True)
+                axis[1, 0].set_ylabel('x/y Velocity')
+
+                axis[1, 1].plot(np.arange(episode) + 1, [obsv[3] for obsv in final_obsvs], 'k.')
+                axis[1, 1].grid(True)
+
+                axis[2, 0].plot(np.arange(episode) + 1, [obsv[4] for obsv in final_obsvs], 'k.')
+                axis[2, 0].grid(True)
+                axis[2, 0].set_ylabel('phi Angle/Velocity')
+                axis[2, 0].set_xlabel('Episodes')
+
+                axis[2, 1].plot(np.arange(episode) + 1, [obsv[5] for obsv in final_obsvs], 'k.')
+                axis[2, 1].grid(True)
+                axis[2, 1].set_xlabel('Episodes')
+
+                plt.savefig('stats/' + prefix + f'_final_obsvs_e{episode}.png')
+
+                plt.close(figure)
+
+                print(f'Episode {episode} saved')
+
+        self._cleanup()
+    
+    def _init_time(self):
+        self.old_time_ms = pygame.time.get_ticks()
+
+    def _delay(self, dt):
+        new_time_ms = pygame.time.get_ticks()
+        delta_time_ms = new_time_ms - self.old_time_ms
+        self.old_time_ms = new_time_ms
+
+        d_d_time_ms = int(dt * 1000 - delta_time_ms)
+
+        if d_d_time_ms > 0:
+            pygame.time.delay(d_d_time_ms)
+
+if __name__ == "__main__":
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f'Using device {device}')
+
+    game = Game(render=True, agent_play=True, agent_train=True, agent_file='rocket_game_hover', save_episodes=100, step_limit=2000, device=device)
+    game.play()
